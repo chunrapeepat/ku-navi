ba23bcb901283dbc2814fdc71b52a6686febc2d0
Chun Rapeepat <chunza2542@Chuns-MacBook-Pro.local>
2017-10-06 15:14:10 +0700

    Finish search container mockup

---
+++ src/containers/search.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const SearchInput = styled.input`
+  outline: none;
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 15px 20px;
+  font-size: 18px;
+  font-family: 'Kanit', sans-serif;
+  border-bottom: 1px solid #ccc;
+`
+
+const Background = styled.div`
+  position: absolute;
+  top: 58px;
+  left: 0;
+  height: calc(100% - 58px);
+  right: 0;
+  background: rgba(0,0,0,0.5);
+  display: ${props => (props.display) ? 'block' : 'none'};
+`
+
+export default class extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      display: false
+    }
+  }
+
+  openBackground(){
+    this.setState({ display: true })
+  }
+
+  closeBackground(){
+    this.setState({ display: false })
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchInput
+          onClick={() => this.openBackground()}
+          placeholder='ค้นหาสถานที่อื่นๆ...'/>
+        <Background
+          onClick={() => this.closeBackground()}
+          display={this.state.display}/>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+import SearchContainer from './containers/search'
 import './design.css'
 
 class App extends React.Component {
   render(){
     return (
       <div>
-        hi Chunop
+        <SearchContainer />
       </div>
     )
   }
